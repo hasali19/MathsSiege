a5cca93bf550d29e734d2d3cc1410a1b0cb1569e
Hasan Ali <hasan2040@outlook.com>
2018-01-20 18:15:30 +0000

    Add nav cards to homepage

--- MathsSiege.Server/Pages/Index.cshtml.cs
+++ MathsSiege.Server/Pages/Index.cshtml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MathsSiege.Server.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
-
-        }
     }
 }
