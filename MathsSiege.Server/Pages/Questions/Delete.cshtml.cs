c93fdce5dc56f41ed4d746937ecbbd4745041d28
Hasan Ali <hasan2040@outlook.com>
2018-01-21 20:51:42 +0000

    Add question deletion page

---
+++ MathsSiege.Server/Pages/Questions/Delete.cshtml.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MathsSiege.Server.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MathsSiege.Server.Pages.Questions
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly IQuestionRepository questionRepository;
+
+        public DeleteModel(IQuestionRepository questionRepository)
+        {
+            this.questionRepository = questionRepository;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            var question = await questionRepository.GetQuestionAsync(id);
+
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            await questionRepository.DeleteQuestionAsync(id);
+
+            return RedirectToPage("/Questions/Index");
+        }
+    }
+}
