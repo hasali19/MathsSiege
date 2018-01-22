af7dfdee087764652950b08f46c8ad9da047b016
Hasan Ali <hasan2040@outlook.com>
2018-01-22 20:35:24 +0000

    Add game session api endpoint

---
+++ MathsSiege.Server/Controllers/GameSessionController.cs
@@ -0,0 +1,44 @@
+﻿using MathsSiege.Models;
+using MathsSiege.Server.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MathsSiege.Server.Controllers
+{
+    [Route("Api/GameSessions")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class GameSessionController : Controller
+    {
+        private readonly IUserRepository userRepository;
+        private readonly IGameSessionRepository gameSessionRepository;
+
+        public GameSessionController(IUserRepository userRepository, IGameSessionRepository gameSessionRepository)
+        {
+            this.userRepository = userRepository;
+            this.gameSessionRepository = gameSessionRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] GameSession gameSession)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await userRepository.GetUserAsync(User.Identity.Name);
+
+            if (user == null)
+            {
+                return NotFound(new { Message = "An error occurred while retrieving the user." });
+            }
+
+            gameSession.User = user;
+            await gameSessionRepository.AddGameSessionAsync(gameSession);
+            
+            return Ok(gameSession);
+        }
+    }
+}
