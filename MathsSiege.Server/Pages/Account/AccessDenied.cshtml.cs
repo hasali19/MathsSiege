7ac6290a576b0b0c8603395bcba825fdd77b481f
hasali19 <hasali2070@gmail.com>
2018-02-28 21:23:42 +0000

    Redirect student users to account page when requesting homepage

---
+++ MathsSiege.Server/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MathsSiege.Server.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public IActionResult OnGet(string returnUrl)
+        {
+            if (string.IsNullOrEmpty(returnUrl) || returnUrl == "/")
+            {
+                return RedirectToPage("/Account/Index");
+            }
+
+            return Page();
+        }
+    }
+}
